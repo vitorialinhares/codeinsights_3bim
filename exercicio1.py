2929bed239fa37052ea11eb59b86337a9f046cf0
vitorialinhares <vitoriamontenegro8877@gmail.com>
2025-09-17 20:25:06 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,24 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V1.1
+
+
+#begin_inputs
+num4 = 4
+num6 = 6
+num20 = 20
+#end_inputs
+
+soma = num6 + num4
+print(soma)
+dif = num4 - num6
+print(dif)
+mult = num6 * num4
+print(mult)
+
+div = num20 / num6
+print(div)
+div_inteira = num20 // num6
+print(div_inteira)
+resto_div = num20 % num6
+print(resto_div)
---
+++ exercicio21.py
@@ -0,0 +1,22 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V2.1
+
+
+#begin_inputs
+a = 1
+b = 2
+x = a
+y = b
+c = 3
+z = 4
+#end_inputs
+
+#os prints vão a partir daqui
+print("{} e {} sao {} e {}" .format(x, y, a, b))
+
+print("Total: {}" .format(x + y))
+
+print("{} {} {} sao numeros inteiros" .format(a, b, c))
+
+print("{} + 0 = {}" .format(z, z))
