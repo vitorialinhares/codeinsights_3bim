29541350455348584fa3a08321c81f8ed8a8316a
vitorialinhares <vitoriamontenegro8877@gmail.com>
2025-09-17 17:53:12 -0300

    Create exercico22.py

---
+++ exercico22.py
@@ -0,0 +1,12 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V2.2
+
+
+#begin_inputs
+dia = 11
+mes = 2
+ano = 2009
+#end_inputs
+
+print("{}/{}/{}" .format(dia, mes, ano))
