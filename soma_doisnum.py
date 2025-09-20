22f5f698edf4e1058204e228a64f9a8424fa5753
vitorialinhares <vitoriamontenegro8877@gmail.com>
2025-09-20 14:03:11 -0300

    Create soma_doisnum.py

---
+++ soma_doisnum.py
@@ -0,0 +1,13 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid 0
+
+
+#begin_inputs
+num1 = 2
+num2 = 3
+#end_inputs
+
+# Faça o processamento e a exibição da saída a partir daqui
+soma = num1 + num2
+print(soma)
