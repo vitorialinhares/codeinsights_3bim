54d9c27cf49a5a747a3457809252fa3037109a87
vitorialinhares <vitoriamontenegro8877@gmail.com>
2025-09-21 23:42:46 -0300

    Create exercicio31.py

---
+++ exercicio31.py
@@ -0,0 +1,14 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V3.1
+
+
+#begin_inputs
+
+# Isso está aqui apenas para grantir que os testes automáticos funcionem
+# NÃO mexa aqui.
+n = 99
+#end_inputs
+
+for i in range(99, 251):
+    print(f"{i} bugs no software, pegue um deles e conserte...")
