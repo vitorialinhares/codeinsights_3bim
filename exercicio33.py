11c8db968f00b9de29f07521d0b40fa96cf01ba6
Vitória <vitoriamontenegro8877@gmail.com>
2025-09-23 14:24:46 -0300

    Add files via upload

---
+++ exercicio33.py
@@ -0,0 +1,12 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V3.3
+
+
+#begin_inputs
+n=2004
+#end_inputs
+
+for i in range(2004, 2097):
+    if i % 4 == 0:
+        print(i)
---
+++ exercicio34.py
@@ -0,0 +1,13 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V3.4
+
+
+#begin_inputs
+# programa sem entrada (input)
+#end_inputs
+for ano in range(2017,2021):
+    for mes in range(1,13):
+        for dia in range(1,31):
+            print("{}/{}/{}" .format(dia,mes,ano))
+	
