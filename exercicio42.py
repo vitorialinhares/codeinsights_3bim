df4e247227ceb7b9734931dfbc3f85400309c847
Vitória <vitoriamontenegro8877@gmail.com>
2025-09-30 14:11:33 -0300

    Add files via upload

---
+++ exercicio42.py
@@ -0,0 +1,15 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V4.2
+
+
+#begin_inputs
+ #mantenha esse trecho. o input será manipulado aqui.
+contador = 0
+for n in range(10):
+    idade = int(input())
+    if idade >= 18:
+        contador += 1
+#end_inputs
+print (contador)
+	
---
+++ exercicio43.py
@@ -0,0 +1,14 @@
+# @cikey 3749a643a52f3d6250bf1405b3469420
+# @sid 20251174010010
+# @aid V4.3
+
+
+#begin_inputs
+mes = int(input())
+#end_inputs
+lista_mes = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+mes_lista = mes -1
+if 1 < 1 or mes > 12:
+    print("mes invalido")
+else:
+    print(lista_mes[mes_lista])
